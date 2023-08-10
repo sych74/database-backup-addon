6dab88187b24cd19f5905f6b11e88daf4048577f
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2023-08-10 18:33:18 +0300

    Update backup-logic.sh

--- scripts/backup-logic.sh
+++ scripts/backup-logic.sh
@@ -13,7 +13,14 @@ BACKUP_ADDON_REPO=$(echo ${BASE_URL}|sed 's|https:\/\/raw.githubusercontent.com\
 BACKUP_ADDON_BRANCH=$(echo ${BASE_URL}|sed 's|https:\/\/raw.githubusercontent.com\/||'|awk -F / '{print $3}')
 BACKUP_ADDON_COMMIT_ID=$(git ls-remote https://github.com/${BACKUP_ADDON_REPO}.git | grep "/${BACKUP_ADDON_BRANCH}$" | awk '{print $1}')
 
-which restic || { yum-config-manager --disable nodesource; yum -y install restic; }
+if which restic; then
+    true
+else
+    yum-config-manager --disable nodesource; 
+    yum-config-manager --enable copr:copr.fedorainfracloud.org:copart:restic
+    yum -y install restic;
+    yum-config-manager --disable copr:copr.fedorainfracloud.org:copart:restic
+fi
 
 if [ "$COMPUTE_TYPE" == "redis" ]; then
     if grep -q '^cluster-enabled yes' /etc/redis.conf; then
