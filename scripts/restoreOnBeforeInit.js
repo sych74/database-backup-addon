71e44fca91d409d1320c90036a47e6a6fb3c6043
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2024-04-08 18:14:38 +0300

    JE-70937 [DB Backup/Restore add-on]: Restoring to Galera cluster is failed

--- scripts/restoreOnBeforeInit.js
+++ scripts/restoreOnBeforeInit.js
@@ -3,6 +3,9 @@ var storage_unavailable_markup = "";
 var storageInfo = getStorageNodeid();
 var storageEnvDomain = storageInfo.storageEnvShortName;
 var storageEnvMasterId = storageInfo.storageNodeId;
+var checkSchemaCommand = "if grep -q '^SCHEME=' /.jelenv; then echo true; else echo false; fi";
+var mysql_cluster_markup = "Be careful when restoring the dump from another DB environment (or environment with another replication schema) to the replicated MySQL/MariaDB/Percona solution.";
+var recovery_addon_markup = "Please use Database Corruption Diagnostic add-on for check after restore, and Database Recovery Add-on for fix if it is needed.";
 
 resp = jelastic.env.control.GetEnvInfo(storageEnvDomain, session);
 if (resp.result != 0 && resp.result != 11) return resp;
@@ -12,7 +15,7 @@ if (resp.result == 11) {
     var baseUrl = jps.baseUrl;
     var updateResticOnStorageCommand = "wget --tries=10 -O /tmp/installUpdateRestic " + baseUrl + "/scripts/installUpdateRestic && mv -f /tmp/installUpdateRestic /usr/sbin/installUpdateRestic && chmod +x /usr/sbin/installUpdateRestic && /usr/sbin/installUpdateRestic";
     var respUpdate = api.env.control.ExecCmdById(storageEnvDomain, session, storageEnvMasterId, toJSON([{"command": updateResticOnStorageCommand, "params": ""}]), false, "root");
-    if (respUpdate.result != 0) return resp;
+    if (respUpdate.result != 0) return respUpdate;
     var backups = jelastic.env.control.ExecCmdById(storageEnvDomain, session, storageEnvMasterId, toJSON([{"command": "/root/getBackupsAllEnvs.sh", "params": ""}]), false, "root").responses[0].out;
     var backupList = toNative(new JSONObject(String(backups)));
     var envs = prepareEnvs(backupList.envs);
@@ -20,6 +23,9 @@ if (resp.result == 11) {
 } else {
     storage_unavailable_markup = "Storage environment " + storageEnvDomain + " is unavailable (stopped/sleeping).";
 }
+
+var checkSchema = api.env.control.ExecCmdById("${env.name}", session, ${targetNodes.master.id}, toJSON([{"command": checkSchemaCommand, "params": ""}]), false, "root");
+if (checkSchema.result != 0) return checkSchema;
       
 function getStorageNodeid(){
     var storageEnv = '${settings.storageName}'
@@ -81,7 +87,15 @@ if (storage_unavailable_markup === "") {
             "dependsOn": {
                 "backupedEnvName" : backups
             }
-        })
+        });
+    if (checkSchema.responses[0].out == "true") {
+        settings.fields.push(
+            {"type": "displayfield", "cls": "warning", "height": 30, "hideLabel": true, "markup": mysql_cluster_markup}
+        );
+	settings.fields.push(
+            {"type": "displayfield", "cls": "warning", "height": 30, "hideLabel": true, "markup": recovery_addon_markup}
+        );
+    }	
 } else {
     settings.fields.push(
         {"type": "displayfield", "cls": "warning", "height": 30, "hideLabel": true, "markup": storage_unavailable_markup}
