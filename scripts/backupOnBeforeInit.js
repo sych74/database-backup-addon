6055955066f4c04e95fb9c70a68671c28a1b2b68
Henadii Sychevskyi <sych74@gmail.com>
2024-05-24 09:05:57 +0200

    JE-71677

--- scripts/backupOnBeforeInit.js
+++ scripts/backupOnBeforeInit.js
@@ -4,7 +4,7 @@ var envs = [];
 var nodes = {};
 for (var i = 0, envInfo, env; envInfo = resp.infos[i]; i++) {
     if  (envInfo.envGroups.includes("Backup storage nodes") || 
-        (envInfo.env.properties && envInfo.env.properties.projecScope == "backup")) {
+        (envInfo.env.properties && envInfo.env.properties.projectScope == "backup")) {
         env = envInfo.env
         if (env.status == 1) {
             for (var j = 0, node; node = envInfo.nodes[j]; j++) {
