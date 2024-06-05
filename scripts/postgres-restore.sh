ea02330ec39afd0af9125448bd02e13cbc440747
Henadii Sychevskyi <sych74@gmail.com>
2024-06-05 16:07:13 +0200

    JE-69780

--- scripts/postgres-restore.sh
+++ scripts/postgres-restore.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 CLIENT_APP="psql"
-SERVER_IP_ADDR=$(ip a | grep -A1 venet0 | grep inet | awk '{print $2}'| sed 's/\/[0-9]*//g' | tail -n 1)
-[ -n "${SERVER_IP_ADDR}" ] || SERVER_IP_ADDR="localhost"
-PGPASSWORD=${2} psql --no-readline -q -U ${1} -d postgres < /root/db_backup.sql;
+PGPASSWORD=${2} ${CLIENT_APP} --no-readline -q -U ${1} -d postgres < /root/db_backup.sql;
