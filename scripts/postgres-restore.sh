72021c13954b8012b800efcbd343b4fc6e197e4a
Henadii Sychevskyi <sych74@gmail.com>
2024-06-12 12:07:24 +0200

    JE-69780

--- scripts/postgres-restore.sh
+++ scripts/postgres-restore.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 CLIENT_APP="psql"
-PGPASSWORD=${2} ${CLIENT_APP} --no-readline -q -U ${1} -d postgres < /root/db_backup.sql;
+ORIG_BACKUP="/root/db_backup.sql"
+TEMP_BACKUP="/tmp/db_backup.sql"
+
+[ -f "$TEMP_BACKUP" ] && rm -f "$TEMP_BACKUP"
+cp "$ORIG_BACKUP" "$TEMP_BACKUP"
+
+sed -i -e '/^CREATE ROLE webadmin/d' \
+       -e '/^CREATE ROLE postgres/d' \
+       -e '/^DROP ROLE IF EXISTS postgres/d' \
+       -e '/^DROP ROLE IF EXISTS webadmin/d' \
+       -e '/^ALTER ROLE postgres WITH SUPERUSER/d' \
+       -e '/^ALTER ROLE webadmin WITH SUPERUSER/d' "$TEMP_BACKUP"
+
+PGPASSWORD=${2} ${CLIENT_APP} --no-readline -q -U ${1} -d postgres < "$TEMP_BACKUP";
+
+[ -f "$TEMP_BACKUP" ] && rm -f "$TEMP_BACKUP"
