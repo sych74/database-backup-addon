a43fa02952420359c6487ffa28d850b3bca2e979
Henadii Sychevskyi <sych74@gmail.com>
2024-10-03 14:36:57 +0200

    Update restote-test.sh

--- scripts/restote-test.sh
+++ scripts/restote-test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 DBUSER=$1
 DBPASSWD=$2
 RESTORE_LOG_FILE=$3
@@ -9,7 +8,6 @@ PITR=$4
 DUMP_BACKUP_DIR=/root/backup/dump
 BINLOGS_BACKUP_DIR=/root/backup/binlogs
 SQL_DUMP_NAME=db_backup.sql
-
 
 if [ -f /root/.backupedenv ]; then
     ENV_NAME=$(cat /root/.backupedenv)
@@ -61,7 +59,7 @@ function find_closest_snapshot_id_before_time() {
 
 function get_dump_snapshot_id_by_name(){
     local backup_name="$1"
-    local snapshot_id=$(RESTIC_PASSWORD=${ENV_NAME} restic -r /opt/backup/${ENV_NAME} snapshots --json | jq -r '.[] | select(.tags[0] | contains('$backup_name')) | select((.tags[1] | contains("BINLOGS") | not) // true) | .short_id')
+    local snapshot_id=$(GOGC=20 RESTIC_PASSWORD=${ENV_NAME} restic -r /opt/backup/${ENV_NAME} snapshots --json | jq -r '.[] | select(.tags[0] | contains('$backup_name')) | select((.tags[1] | contains("BINLOGS") | not) // true) | .short_id')
     if [[ $? -ne 0 || -z "$snapshot_id" ]]; then
         echo $(date) ${ENV_NAME} "Error: Failed to get DB dump snapshot ID" | tee -a ${RESTORE_LOG_FILE}
         exit 1
@@ -70,15 +68,36 @@ function get_dump_snapshot_id_by_name(){
     echo $snapshot_id
 }
 
-function get_dump_snapshot_id_by_time(){
-    local backup_time="$1"
-    
+function get_binlog_snapshot_id_by_name(){
+    local backup_name="$1"
+    local snapshot_id=$(GOGC=20 RESTIC_PASSWORD=${ENV_NAME} restic -r /opt/backup/${ENV_NAME} snapshots --tag "BINLOGS" --json | jq -r '.[] | select(.tags[0] | contains('$backup_name')) | .short_id')
+    if [[ $? -ne 0 || -z "$snapshot_id" ]]; then
+        echo $(date) ${ENV_NAME} "Error: Failed to get DB binlogs snapshot ID" | tee -a ${RESTORE_LOG_FILE}
+        exit 1
+    fi
+    echo $(date) ${ENV_NAME} "Getting DB binlogs snapshot ID: $snapshot_id" >> ${RESTORE_LOG_FILE}
+    echo $snapshot_id
+}
 
+
+function get_snapshot_name_by_id(){
+    local snapshot_id="$1"
+    local snapshot_name=$(GOGC=20 RESTIC_PASSWORD=${ENV_NAME} restic -r /opt/backup/${ENV_NAME} snapshots --json | jq -r --arg id "$snapshot_id" '.[] | select(.short_id == $id) | .tags[0]')
+    if [[ $? -ne 0 || -z "$dump_name" ]]; then
+        echo $(date) ${ENV_NAME} "Error: Failed to get snapshot name for $snapshot_id" | tee -a ${RESTORE_LOG_FILE}
+        exit 1
+    fi
+    echo $(date) ${ENV_NAME} "Getting the snapshot name: ${snapshot_name}" >> ${BACKUP_LOG_FILE}
+    echo ${snapshot_name}
 }
 
 function restore_snapshot_by_id(){
     local snapshot_id="$1"
-    RESTIC_PASSWORD=${ENV_NAME} GOGC=20 restic -r /opt/backup/${ENV_NAME} restore ${snapshot_id} --target / || { echo $(date) ${ENV_NAME} "Error: Failed to restore snapshot ID $snapshot_id" | tee -a ${RESTORE_LOG_FILE}; exit 1; }
+    RESTIC_PASSWORD=${ENV_NAME} GOGC=20 restic -r /opt/backup/${ENV_NAME} restore ${snapshot_id} --target / 
+    if [[ $? -ne 0 ]]; then
+        echo $(date) ${ENV_NAME} "Error: Failed to restore snapshot ID $snapshot_id" | tee -a ${RESTORE_LOG_FILE};
+        exit 1
+    fi
     echo $(date) ${ENV_NAME} "Snapshot ID: $snapshot_id restored successfully" >> ${RESTORE_LOG_FILE}
 }
 
