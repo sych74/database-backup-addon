ff1fd1945ad5c51aacdc788329c101672df49b5e
Henadii Sychevskyi <sych74@gmail.com>
2024-10-04 16:30:39 +0200

    Update restote-test.sh

--- scripts/restote-test.sh
+++ scripts/restote-test.sh
@@ -9,6 +9,8 @@ DUMP_BACKUP_DIR=/root/backup/dump
 BINLOGS_BACKUP_DIR=/root/backup/binlogs
 SQL_DUMP_NAME=db_backup.sql
 
+#rm -rf $DUMP_BACKUP_DIR $BINLOGS_BACKUP_DIR
+
 if [ -f /root/.backupedenv ]; then
     ENV_NAME=$(cat /root/.backupedenv)
 else
@@ -91,7 +93,7 @@ function get_snapshot_name_by_id(){
         echo $(date) ${ENV_NAME} "Error: Failed to get snapshot name for $snapshot_id" | tee -a ${RESTORE_LOG_FILE}
         exit 1
     fi
-    echo $(date) ${ENV_NAME} "Getting the snapshot name: ${snapshot_name}" >> ${BACKUP_LOG_FILE}
+    echo $(date) ${ENV_NAME} "Getting the snapshot name: ${snapshot_name}" >> ${RESTORE_LOG_FILE}
     echo ${snapshot_name}
 }
 
@@ -105,18 +107,56 @@ function restore_snapshot_by_id(){
     echo $(date) ${ENV_NAME} "Snapshot ID: $snapshot_id restored successfully" >> ${RESTORE_LOG_FILE}
 }
 
+function restore_mysql_dump(){
+    if which mariadb 2>/dev/null; then
+        CLIENT_APP="mariadb"
+    else
+        CLIENT_APP="mysql"
+    fi
+    ${CLIENT_APP} -u "${DBUSER}" -p"${DBPASSWD}" < "${DUMP_BACKUP_DIR}/${SQL_DUMP_NAME}"
+    if [[ $? -ne 0 ]]; then
+        echo "$(date) ${ENV_NAME} Error: Failed to restore MySQL dump" | tee -a ${RESTORE_LOG_FILE}
+        exit 1
+    fi
+    echo "$(date) ${ENV_NAME} MySQL dump restored successfully" | tee -a ${RESTORE_LOG_FILE}
+}
+
+function apply_binlogs_until_time(){
+    local stop_time="$1"
+    local binlog_files=($BINLOGS_BACKUP_DIR/mysql-bin.*)
+
+    if which mariadb 2>/dev/null; then
+         BINLOG_APP="mariadb-binlog"
+    else
+         BINLOG_APP="mysqlbinlog"
+    fi
+
+    for binlog_file in "${binlog_files[@]}"; do
+        ${BINLOG_APP} --stop-datetime="${stop_time}" "$binlog_file" | mysql -u "${DBUSER}" -p"${DBPASSWD}"
+        if [[ $? -ne 0 ]]; then
+            echo "$(date) ${ENV_NAME} Error: Failed to apply binlogs from $binlog_file" | tee -a ${RESTORE_LOG_FILE}
+            exit 1
+        fi
+        echo "$(date) ${ENV_NAME} Applied binlogs from $binlog_file until $stop_time" >> ${RESTORE_LOG_FILE}
+    done
+}
+
 function restore_mysql(){
     if [ "$PITR" == "true" ]; then
-        dump_snapshot_id=$(get_snapshot_id_before_time "${PITR_TIME}")
-        dump_snapshot_name=$(get_snapshot_name_by_id "${dump_snapshot_id}")
+#        dump_snapshot_id=$(get_snapshot_id_before_time "${PITR_TIME}")
+#        dump_snapshot_name=$(get_snapshot_name_by_id "${dump_snapshot_id}")
 
-        binlog_snapshot_id=$(get_binlog_snapshot_id_by_name "${dump_snapshot_name}")
+#        binlog_snapshot_id=$(get_binlog_snapshot_id_by_name "${dump_snapshot_name}")
 
-        restore_snapshot_by_id "${dump_snapshot_id}"
-        restore_snapshot_by_id "${binlog_snapshot_id}"
+#        restore_snapshot_by_id "${dump_snapshot_id}"
+        restore_mysql_dump
+
+#        restore_snapshot_by_id "${binlog_snapshot_id}"
+        apply_binlogs_until_time "${PITR_TIME}"
     else
         dump_snapshot_id=$(get_dump_snapshot_id_by_name "${BACKUP_NAME}")
         restore_snapshot_by_id "${dump_snapshot_id}"
+        restore_mysql_dump
     fi
 }
 
