fc9d2a0dd52c4519594d1e82faaa4104c2de49ac
Henadii Sychevskyi <sych74@gmail.com>
2024-12-05 10:45:44 +0100

    JE-73285

--- scripts/mariadb-restore.sh
+++ scripts/mariadb-restore.sh
@@ -10,9 +10,9 @@ else
     CLIENT_APP="mysql"
 fi
 
+${CLIENT_APP} --silent -h ${SERVER_IP_ADDR} -u ${1} -p${2} --force < /root/db_backup.sql
+
 if [ -n "${SCHEME}" ] && [ x"${SCHEME}" == x"galera" ]; then
-    curl --silent https://raw.githubusercontent.com/jelastic-jps/mysql-cluster/master/addons/recovery/scripts/db-recovery.sh > /tmp/db-recovery.sh
-    bash /tmp/db-recovery.sh --scenario restore_galera --from-backup /root/db_backup.sql
-else
-    ${CLIENT_APP} --silent -h ${SERVER_IP_ADDR} -u ${1} -p${2} --force < /root/db_backup.sql
+    curl --silent https://raw.githubusercontent.com/sych74/mysql-cluster/refs/heads/JE-73285/addons/recovery/scripts/db-recovery.sh > /tmp/db-recovery.sh
+    bash /tmp/db-recovery.sh --scenario restore_galera --donor-ip ${SERVER_IP_ADDR}
 fi
